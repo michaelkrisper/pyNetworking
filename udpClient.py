8ded866019e51190630a37426b0232b527bc9e3c
Michael Krisper <michael.krisper@gmail.com>
2014-11-04 00:17:22 +0100

    udp server

--- udpClient.py
+++ udpClient.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 # coding=utf-8
 """
-
+https://www.youtube.com/watch?v=XiVVYfgDolU#t=1344
 """
 
 __author__ = "Michael Krisper"
 __email__ = "michael.krisper@gmail.com"
 
+
+import socket
 
 def main():
     pass
--- udpServer.py
+++ udpServer.py
@@ -1,16 +1,44 @@
 #!/usr/bin/python3
 # coding=utf-8
 """
-
+https://www.youtube.com/watch?v=XiVVYfgDolU#t=1079
 """
 
 __author__ = "Michael Krisper"
 __email__ = "michael.krisper@gmail.com"
 
+import socket
+
 
 def main():
-    pass
+    host = "127.0.0.1"
+    port = 5000
 
+    s = socket.socket(type=socket.SOCK_DGRAM)
+
+    s.bind((host, port))
+
+    try:
+        print("Server started")
+
+        while True:
+            data, addr = s.recvfrom(1024)
+
+            if not data:
+                break
+
+            data = data.encode()
+            print("Message from {}".format(addr))
+            print("from connected user: {}".format(data))
+
+            data = data.upper()
+
+            print("Sending: {}".format(data))
+
+            s.sendto(data, addr)
+
+    finally:
+        s.close()
 
 if __name__ == "__main__":
     main()
