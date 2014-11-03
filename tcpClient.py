3b556f3f57b3b7421e47c7444dce963bf7934415
Michael Krisper <michael.krisper@gmail.com>
2014-11-03 23:51:04 +0100

    Initial commit

---
+++ tcpClient.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding=utf-8
+"""
+TcpClient
+
+"""
+
+__author__ = "Michael Krisper"
+__email__ = "michael.krisper@gmail.com"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ tcpServer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# coding=utf-8
+"""
+TcpServer
+https://docs.python.org/3.4/library/queue.html#module-queue
+"""
+
+__author__ = "Michael Krisper"
+__email__ = "michael.krisper@gmail.com"
+
+
+import socket
+
+
+
+def main():
+    host = "127.0.0.1"
+    port = 5000
+
+    s = socket.socket()
+    s.bind((host, port))
+
+    s.listen(1)
+    conn, addr = s.accept()
+    print("Connection from : {}".format(addr))
+
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+
+        print("from connected user: {}".format(data))
+        data = str(data).upper()
+
+        print("sending: {}".format(data))
+        conn.send(data)
+    conn.close()
+
+if __name__ == "__main__":
+    main()
