256c749c1da944e543b42701ba40883cd42add6c
Michael Krisper <michael.krisper@gmail.com>
2014-11-04 00:12:28 +0100

    udp

---
+++ udpClient.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# coding=utf-8
+"""
+
+"""
+
+__author__ = "Michael Krisper"
+__email__ = "michael.krisper@gmail.com"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
